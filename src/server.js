b13d83e2bc9494256d0f39035d689d9b8bfacbdd
Marcelle <marcellealves.dev@gmail.com>
2026-01-25 18:46:48 -0300

    feat: basic HTTP routing by inspecting request method and URL
    Add manual route handling in createServer callback by destructuring
    req.method and req.url - demonstrating how Node.js http module works
    at its core

--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ import http from 'node:http'
 const PORT = 3333
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if(method === 'GET' && url === '/users') {
+    // Early return
+    return res.end('Users list.')
+  } 
+  
+  if (method === 'POST' && url === '/users') {
+    return res.end('New user.')
+  }
+  
   return res.end('Hello, world!')
 })
 
